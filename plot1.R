af92f7a3344b2123570e38aede8405e494f91a3b
Inigo Montoya <imitcs@gmail.com>
2015-06-02 20:55:38 +0200

    Exploratory Data Analysis - Project 1 Submission
    Exploratory Data Analysis - Project 1 Submission
    4 plotN.R f& 4 plotN.png files

---
+++ plot1.R
@@ -0,0 +1,21 @@
+# RAM Required Estimation:
+# 2,075,259 × 9 columns × 8 bytes/numeric = 149,418,648 bytes / 2^20 bytes/MB = 142.50 MB
+# checked with pryr::object_size = 133 MB; mem_used = 152 MB
+
+# Read five lines to calculate classes to accelerate the read.table execution 
+Initial_HPC <- read.table("household_power_consumption.txt", header = TRUE, nrows = 5, sep = ";", na.strings = "?")
+Field_Classes <- sapply(Initial_HPC, class)
+
+HPC <- read.table("household_power_consumption.txt", header = TRUE, colClasses = Field_Classes, sep = ";", na.strings = "?")
+Filtered_HPC = HPC[HPC$Date %in% c("1/2/2007","2/2/2007"),]
+rm(HPC)
+
+# Alternatively in case of RAM problems: 
+# comment the previuos 5 lines and load only the required data using the next 2 lines    
+# library(sqldf)
+# Filtered_HPC <- read.csv.sql("household_power_consumption.txt", header = TRUE, sep = ";", sql = "select * from file where Date in ('1/2/2007','2/2/2007')") 
+
+# Plot Histogram
+png(file = "plot1.png")
+hist(Filtered_HPC$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)" )
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,31 @@
+# RAM Required Estimation:
+# 2,075,259 × 9 columns × 8 bytes/numeric = 149,418,648 bytes / 2^20 bytes/MB = 142.50 MB
+# checked with pryr::object_size = 133 MB; mem_used = 152 MB
+
+# Read five lines to calculate classes to accelerate the read.table execution 
+Initial_HPC <- read.table("household_power_consumption.txt", header = TRUE, nrows = 5, sep = ";", na.strings = "?")
+Field_Classes <- sapply(Initial_HPC, class)
+
+HPC <- read.table("household_power_consumption.txt", header = TRUE, colClasses = Field_Classes, sep = ";", na.strings = "?")
+Filtered_HPC = HPC[HPC$Date %in% c("1/2/2007","2/2/2007"),]
+rm(HPC)
+
+# Alternatively in case of RAM problems: 
+# comment the previuos 5 lines and load only the required data using the next 2 lines    
+# library(sqldf)
+# Filtered_HPC <- read.csv.sql("household_power_consumption.txt", header = TRUE, sep = ";", sql = "select * from file where Date in ('1/2/2007','2/2/2007')") 
+
+x_axis = strptime(paste(Filtered_HPC$Date,Filtered_HPC$Time), format ="%d/%m/%Y %H:%M:%S")
+png(file = "plot3.png")
+
+plot(x_axis, Filtered_HPC$Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering", )
+par(new=TRUE)
+points(x_axis, Filtered_HPC$Sub_metering_2, type = "l", col = "red")
+points(x_axis, Filtered_HPC$Sub_metering_3, type = "l", col = "blue")
+legend("topright", 
+       pch = c(NA, NA, NA),
+       col = c("black","red","blue"), 
+       legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       lty = c(1,1,1))
+
+dev.off()
